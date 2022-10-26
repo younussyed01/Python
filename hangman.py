0fa22c5e2ac572e331aa941a82e0770378a40725
younussyed01 <younus.syed01@utrgv.edu>
2022-10-26 18:47:12 -0500

    commit

--- hangman.py
+++ hangman.py
@@ -1,7 +1,29 @@
-# get the answer
 import re
-answer = "What's Up, Doc?"
+import random
+#getting the answer.
+
+pool_file = open("Coding_projects_cs/hangman-sample-answer-pool.txt")
+
+pool_answers = []
+
+pool_answers_line = pool_file.readline()
+
+while pool_answers_line:
+  pool_answers.append(pool_answers_line)
+
+  pool_answers_line = pool_file.readline()
+pool_file.close()
+
+answer = random.choice(pool_answers)
 answer = answer.upper()
+
+#game setup.
+
+num_of_incorrect_guesses = 5
+
+answer_guessed = []
+
+
 
 answer_guessed = []
 
@@ -64,5 +86,6 @@ while current_incorrect_guesses < num_incorrect_guesses and False in answer_gues
     #post game
 if current_incorrect_guesses < num_incorrect_guesses:
     print("Congratulations, you won!")
+    print(f"The answer was {answer}")
 else:
     print(f"sorry, you lost. the answer was {answer}")
